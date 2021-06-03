4e72771bc1a695280fdc48b933f5db883a24db4d
unknown <matias314159@gmail.com>
2021-06-03 08:58:17 -0300

    example app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Result from './components/Result';
+import MathOperations from './components/MathOperations';
+import Functions from './components/Funcitons';
+import Numbers from './components/Numbers'
+
 import './App.css';
 
-function App() {
+
+const App = () => {
+  /****SIN ARRAY DESTRUCTURING
+  const arrayFuncionModTexto = useState("");
+
+  const texto = arrayFuncionModTexto[0]//valor inicial que tenemos
+
+  const 0 = arrayFuncionModTexto[1]//funcion que me permite modificar el valor
+  ****/
+
+  //CON ARRAY STRUCTURING
+  const [stack, set_stack] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="react-calculator">
+      <Result value={stack}/>
+
+      <Numbers
+        onClickNumber={number =>{
+          console.log(number);
+          set_stack(`${stack}${number}`)//funcion devuelta por useState
+        }}
+      />
+
+      <Functions 
+        onContentClear={()=>{
+          console.log("Clear")
+          set_stack("")
+        }}
+        onDelete={()=>{
+          if(stack.length > 0){
+            console.log("<- borrando");
+            const newStack = stack.substring(0, stack.length - 1)
+            set_stack(newStack)
+          }
+        }}
+      />
+      <MathOperations 
+        onClickOperation={Operation => {
+          if(stack.length > 0){  
+            console.log(Operation)
+            set_stack(`${stack}${Operation}`)
+          }
+        }}
+        onClickEqual={Equal => {
+          if(stack.length > 0){
+            console.log(Equal)
+            set_stack(`${stack}${Equal}`)
+            // eslint-disable-next-line
+            let resultado = eval(stack);
+            set_stack(resultado.toString())
+          }  
+        }}
+      />
     </div>
   );
 }
